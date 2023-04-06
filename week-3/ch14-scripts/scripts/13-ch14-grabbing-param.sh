a92260f44ebcd9f5515c500c6632b280686210a0
husein-bajrektarevic <bytebridger@localhost.localdomain>
2023-04-06 10:57:30 +0200

    added notes and screenshots related to task no. 6

--- week-3/ch14-scripts/scripts/13-ch14-grabbing-param.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# testing $* and $@
-#
-echo
-echo "Using the \$* method: $*"
-echo
-echo "Using the \$@ method: $@"
-
-# $ ./test11.sh rich barbara katie jessica
--- week-3/ch15-scripts/scripts/01-ch15-temp-redirections.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# testing STDERR messages
-echo "This is an error" >&2
-echo "This is normal output"
-
-# cat script.sh
-# to run the script ./script.sh
-# there should be no difference
